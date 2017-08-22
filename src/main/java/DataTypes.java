40da67efb88fa3a7255f1b55ba65c3d4711fd2a7
Kamil Staszewski <kamil.staszewski@yahoo.com>
2017-08-22 19:17:01 +0200

    Hello world

---
+++ src/main/java/DataTypes.java
@@ -0,0 +1,12 @@
+/**
+ * Created by GK on 22.08.2017.
+ */
+public class DataTypes {
+
+    public static void main(String[] args)
+
+    {
+        System.out.println("Witaj świecie!");
+    }
+
+}
