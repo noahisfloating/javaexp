17713ea6db20e382b82aca0bd098848987d1c93b
Kamil Staszewski <kamil.staszewski@yahoo.com>
2018-10-06 09:50:53 +0200

    Added lines for welcome message

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,25 @@
 /**
  * Created by GK on 23.08.2017.
  */
+
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        JFrame frame = new JFrame("Powitanie");
+
+        String htmlText = "<html><font size=+3>" + "Witaj <font color=red><b>Javo</b></font><br>" + "<font color=blue>... A witaj!</font></html>";
+
+        Icon icon = new ImageIcon("java_logo.png");
+        JLabel label = new JLabel(htmlText, icon, JLabel.CENTER);
+        frame.add(label);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
 
         DataTypes.process();
 
